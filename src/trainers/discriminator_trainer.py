4ec2bc8324453fd22d7710f5ee87e946d5987f73
mohamed ali <muhamedali166@gmail.com>
2019-07-01 19:40:53 +0200

    Updating model.

--- src/trainers/discriminator_trainer.py
+++ src/trainers/discriminator_trainer.py
@@ -34,16 +34,16 @@ class DiscriminatorTrainer(BaseTrain):
     def train_step(self):
         batch_x, batch_y = self.data.next_batch(batch_type="train")
         feed_dict = {self.model.x1: batch_x[:, 0], self.model.x2: batch_x[:, 1],
-                     self.model.y: batch_y, self.model.is_training: 1,
-                     self.model.hold_prob: 0.5}
+                     self.model.y: batch_y, self.model.is_training: 1, self.model.hold_prob_conv: 0.25,
+                     self.model.hold_prob_fc: 0.5}
 
         # Run training for step first with dropout then calculate loss and acc without dropout.
         self.sess.run([self.model.train_step, self.model.cross_entropy, self.model.accuracy],
                       feed_dict=feed_dict)
         # Run without dropout
         feed_dict = {self.model.x1: batch_x[:, 0], self.model.x2: batch_x[:, 1],
-                     self.model.y: batch_y, self.model.is_training: 0,
-                     self.model.hold_prob: 1.0}
+                     self.model.y: batch_y, self.model.is_training: 0, self.model.hold_prob_conv: 1.0,
+                     self.model.hold_prob_fc: 1.0}
         loss, acc = self.sess.run([self.model.cross_entropy, self.model.accuracy],
                                   feed_dict=feed_dict)
         return loss, acc
@@ -65,7 +65,8 @@ class DiscriminatorTrainer(BaseTrain):
         batch_x, batch_y = self.data.next_batch(batch_type="val")
         feed_dict = {self.model.x1: batch_x[:, 0],
                      self.model.x2: batch_x[:, 1], self.model.y: batch_y, self.model.is_training: 0,
-                     self.model.hold_prob: 1.0}
+                     self.model.hold_prob_conv: 1.0,
+                     self.model.hold_prob_fc: 1.0}
 
         loss, acc = self.sess.run([self.model.cross_entropy, self.model.accuracy],
                                   feed_dict=feed_dict)
