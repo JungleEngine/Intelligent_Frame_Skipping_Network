8b96ef430a3e7dbe0df4886e03c51a441668ff71
MohamedAli1995 <muhamedali166@gmail.com>
2019-07-02 16:44:21 +0200

    Update discriminator_model.py

--- src/models/discriminator_model.py
+++ src/models/discriminator_model.py
@@ -71,22 +71,22 @@ class DiscriminatorModel(BaseModel):
         self.hold_prob_conv = tf.placeholder(tf.float32, name="hold_prob_conv")
         self.hold_prob_fc = tf.placeholder(tf.float32, name="hold_prob_fc")
 
-        convo_1 = self.__conv_bn_layer(concat, shape=[5, 5, 6, 32])
+        convo_1 = self.__conv_bn_layer(concat, shape=[5, 5, 6, 8])
         dropout_1 = tf.nn.dropout(convo_1, self.hold_prob_conv)
         convo_1_pooling = self.__average_pool_2d(dropout_1)
         
-        convo_2 = self.__conv_bn_layer(convo_1_pooling, shape=[3, 3, 32, 64])
+        convo_2 = self.__conv_bn_layer(convo_1_pooling, shape=[3, 3, 8, 16])
         dropout_2 = tf.nn.dropout(convo_2, self.hold_prob_conv)
         convo_2_pooling = self.__average_pool_2d(dropout_2)
 
-        convo_3 = self.__conv_bn_layer(convo_2_pooling, shape=[3, 3, 64, 128])
+        convo_3 = self.__conv_bn_layer(convo_2_pooling, shape=[3, 3, 16, 32])
         dropout_3 = tf.nn.dropout(convo_3, self.hold_prob_conv)
         convo_3_pooling = self.__average_pool_2d(dropout_3)
 
         flattened = tf.reshape(convo_3_pooling,
-                               [-1, 32 * 32 * 128])
+                               [-1, 32 * 32 * 32])
 
-        full_layer_1 = self.__normal_full_layer(flattened, 128)
+        full_layer_1 = self.__normal_full_layer(flattened, 64)
         batch_norm_6 = self.__batch_norm(full_layer_1)
         full_dropout_1 = tf.nn.dropout(batch_norm_6, self.hold_prob_fc)
 
